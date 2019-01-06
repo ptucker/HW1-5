4d1542050a9f5716e3dc9570189ce1df45a2b061
Peter Tucker <ptucker@whitworth.edu>
2019-01-06 13:57:45 -0800

    feedback

--- HW 1/EX1_4_BMI/bmi.java
+++ HW 1/EX1_4_BMI/bmi.java
@@ -7,12 +7,16 @@ public class bmi {
         double weight; // kilograms
         
         do {
+            //PT -- take care with your indenting.
         System.out.printf("Enter your height in inches: ");  // Checks your height
+            //PT -- why nextInt when you're reading into a double? -1
         height = input.nextInt();
         System.out.printf("Enter your weight in lbs: "); // Checks your weight
         weight = input.nextInt();
         } while (height < 0 || weight < 0);
         
+        //PT -- what are these numbers? 
+        //PT -- better for readability to name these: private static final POUND_TO_KG = 0.453592;
         weight = weight*0.453592;  // Does the math to find height and weight
         height = height*0.0254;
         
